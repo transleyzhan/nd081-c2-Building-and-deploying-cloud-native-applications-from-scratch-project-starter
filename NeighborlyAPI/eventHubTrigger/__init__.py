6c7add292a831e257c90300b59398625d5351e70
Ma Xuân Ngọc Trân <tranmxn@gmail.com>
2024-10-20 16:50:05 +0700

    NeighborlyAPI Update eventHub

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -3,23 +3,18 @@ import logging
 import azure.functions as func
 
 
-def main(event: func.EventGridEvent):
+def main(event: func.EventHubEvent):
+    body = event.get_body().decode()
+    
+    logging.info(f"Function triggered to process a message: {body}")
+    logging.info(f"  EnqueuedTimeUtc = {event.enqueued_time}")
+    logging.info(f"  SequenceNumber = {event.sequence_number}")
+    logging.info(f"  Offset = {event.offset}")
 
-	logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
+    result = json.loads(body)
+    logging.info("Python EventGrid trigger processed an event: {}".format(result))
 
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
-
-
-    logging.info('Python EventGrid trigger processed an event: %s', result)
-
-
-
+    # Metadata
+    for key in event.metadata:
+        logging.info(f"Python EventGrid trigger processed an event: {key}")
+        logging.info(f"Metadata: {key} = {event.metadata[key]}")
